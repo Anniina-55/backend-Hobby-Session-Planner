6f7f45021fff978d90400fdef95c0729aea83344
Anniina-55 <anniina.leikas55@gmail.com>
2025-10-26 00:15:51 +0300

    Updated pool initialization

--- database.js
+++ database.js
@@ -8,7 +8,11 @@ dotenv.config(); // loads the .env file into Node.js environment variables
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false },
 });
 
@@ -19,7 +23,7 @@ export async function initializeDB() {
   // but the function itself waits for it to complete
   await pool.query(`
     CREATE TABLE IF NOT EXISTS sessions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      id SERIAL PRIMARY KEY,
       title TEXT NOT NULL,
       description TEXT,
       date TEXT NOT NULL,
@@ -29,17 +33,16 @@ export async function initializeDB() {
       managementCode TEXT UNIQUE NOT NULL,
       location TEXT NOT NULL,
       inviteToken TEXT UNIQUE
-    )
-  `);
+    )`
+  );
 
   //table for attendances
   await pool.query(`
     CREATE TABLE IF NOT EXISTS attendances (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      sessionId INTEGER NOT NULL,
+      id SERIAL PRIMARY KEY,
+      sessionId INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
       name TEXT,
-      attendanceCode TEXT UNIQUE NOT NULL,
-      FOREIGN KEY(sessionId) REFERENCES sessions(id) ON DELETE CASCADE 
+      attendanceCode TEXT UNIQUE NOT NULL
     )
   `);
 
